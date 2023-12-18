ab9719c24f076540554e98ad78acff85a2417c8b
Marek Zakovic <marek.zakovic@freelance.cz>
2023-12-18 12:27:08 +0100

    .

---
+++ src/main/java/cz/vut/fit/gja_2023/ServletInitializer.java
@@ -0,0 +1,13 @@
+package cz.vut.fit.gja_2023;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Gja2023Application.class);
+    }
+
+}
---
+++ src/test/java/cz/vut/fit/gja_2023/Gja2023ApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.vut.fit.gja_2023;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Gja2023ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
