e9b5a67153319c008c8764842b257afd7a4e22a3
Marek Zakovic <marek.zakovic@freelance.cz>
2023-12-18 13:27:39 +0100

    .

--- src/main/java/cz/vut/fit/gja_2023/Gja2023Application.java
+++ src/main/java/cz/vut/fit/gja_2023/Gja2023Application.java
@@ -1,9 +1,16 @@
 package cz.vut.fit.gja_2023;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
-@SpringBootApplication
+import javax.sql.DataSource;
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class Gja2023Application {
 
     public static void main(String[] args) {
---
+++ src/main/java/cz/vut/fit/gja_2023/configs/DBConfig.java
@@ -0,0 +1,25 @@
+package cz.vut.fit.gja_2023.configs;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
+
+public class DBConfig {
+
+    @SuppressWarnings("SpringJavaInjectionPointsAutowiringInspection")
+    @Autowired
+    private Environment env;
+
+    @Bean
+    public DataSource dataSource() {
+        final DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName(env.getProperty("driverClassName"));
+        dataSource.setUrl(env.getProperty("url"));
+        dataSource.setUsername(env.getProperty("user"));
+        dataSource.setPassword(env.getProperty("password"));
+        return dataSource;
+    }
+}
