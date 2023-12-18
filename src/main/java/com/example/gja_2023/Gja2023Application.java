737586e5c55462fc5b8633141f377b3d02e9517a
Marek Zakovic <marek.zakovic@freelance.cz>
2023-12-18 11:48:28 +0100

    [MZ] Project base

---
+++ src/main/java/com/example/gja_2023/Gja2023Application.java
@@ -0,0 +1,13 @@
+package com.example.gja_2023;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Gja2023Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Gja2023Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/gja_2023/Gja2023ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gja_2023;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Gja2023ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
