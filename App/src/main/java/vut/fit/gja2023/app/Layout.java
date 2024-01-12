ebed1b79ff37f38ffa5439cbee2fef5ca2015221
skrasek.michael <skrasek.michal@email.cz>
2024-01-12 02:02:53 +0100

    added layouts, fragments, templates, bootstrap

---
+++ App/src/main/java/vut/fit/gja2023/app/Layout.java
@@ -0,0 +1,7 @@
+package vut.fit.gja2023.app;
+
+public class Layout {
+    public static final String VIEW = "targetView";
+
+    public static final String DEFAULT_LAYOUT = "layouts/layout";
+}
--- App/src/main/java/vut/fit/gja2023/app/controller/MainController.java
+++ App/src/main/java/vut/fit/gja2023/app/controller/MainController.java
@@ -2,14 +2,18 @@ package vut.fit.gja2023.app.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import vut.fit.gja2023.app.Layout;
 
 @Controller
 @RequiredArgsConstructor
 public class MainController {
 
     @RequestMapping("/")
-    public String index() {
-        return "index";
+    public String index(Model model) {
+        model.addAttribute(Layout.VIEW, "index");
+
+        return Layout.DEFAULT_LAYOUT;
     }
 }
