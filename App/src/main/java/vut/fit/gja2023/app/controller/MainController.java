6d98cc6a5caa5ef362a233ebede0b64bfb4ecd79
Marek Zakovic <marek.zakovic@freelance.cz>
2024-01-13 23:53:22 +0100

    Connection between app and system manager

--- App/src/main/java/vut/fit/gja2023/app/controller/MainController.java
+++ App/src/main/java/vut/fit/gja2023/app/controller/MainController.java
@@ -1,12 +1,21 @@
 package vut.fit.gja2023.app.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import vut.fit.gja2023.app.service.SystemManagerService;
 
 @Controller
 @RequiredArgsConstructor
 public class MainController {
+
+    SystemManagerService systemManagerService;
+
+    @Autowired
+    public MainController(SystemManagerService systemManagerService) {
+        this.systemManagerService = systemManagerService;
+    }
 
     @RequestMapping("/")
     public String index() {
